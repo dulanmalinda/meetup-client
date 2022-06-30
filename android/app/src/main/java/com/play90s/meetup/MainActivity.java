cbb1ac018e28d9c7a44c8bc3b33527f940360384
dulan malinda <dulanmalinda@gmail.com>
2022-06-30 13:47:08 +0530

    commit 06

---
+++ android/app/src/main/java/com/play90s/meetup/MainActivity.java
@@ -0,0 +1,5 @@
+package com.play90s.meetup;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.play90s.meetup',
+  appName: 'meetup',
+  webDir: 'dist/meetup',
+  bundledWebRuntime: false
+};
+
+export default config;
